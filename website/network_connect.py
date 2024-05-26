1cad77b928567b736becbcdd14934daf22ee71dd
Mads25O <mo.overgaard@live.dk>
2024-05-26 03:53:39 +0200

    Updated network_connect.py

--- website/network_connect.py
+++ website/network_connect.py
@@ -1,13 +1,27 @@
 from netmiko import ConnectHandler
+import napalm
 
-connection = ConnectHandler(host='192.168.20.5', port=22,
-                            username='6c', password='6c',
-                            device_type='cisco_ios')
+ip_address = '192.168.87.20'
+
+driver = napalm.get_network_driver('ios')
+device = driver(
+    hostname = ip_address,
+    username = '6c',
+    password = '6c'
+)
+device.open()
 
 
-output = connection.send_command('show running-config', use_textfsm=True)
+connection = ConnectHandler(
+    host = ip_address,
+    port = 22,
+    username = 'mads', 
+    password = 'mads',
+    device_type = 'cisco_ios')
+
+output = connection.send_command('show ip int brief')
 
 print(output)
-#print(connection.find_prompt())
+print(connection.find_prompt())
 
 connection.disconnect()
